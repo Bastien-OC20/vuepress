7c791a644d3c60405fa669f800e6937f953422ab
sebastien <bastien.rapuzzi@gmail.com>
2023-08-15 21:10:34 +0200

    🔧 : Changement de base de données mariadb => postgresql

--- backend/Database/dbConnexion.js
+++ backend/Database/dbConnexion.js
@@ -1,10 +1,14 @@
 const Sequelize = require('sequelize');
 require('dotenv').config()
 
-const sequelize = new Sequelize(process.env.DB_DATANAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT
+const sequelize = new Sequelize(`postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}.oregon-postgres.render.com/${process.env.DB_DATANAME}`, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false // Vous devrez peut-être ajuster cela en fonction de vos besoins de sécurité
+        }
+    }
 });
 
 sequelize
